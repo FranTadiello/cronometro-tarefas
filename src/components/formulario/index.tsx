4cdff691908ec7ca3a0bf7a7a185749c76080fb9
Franciele Tadiello <franciele.p.t@gmail.com>
2023-06-26 11:27:51 -0300

    Adição de states e atualização de tarefas

--- src/components/formulario/index.tsx
+++ src/components/formulario/index.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import Botao from '../botao';
 import style from './Formulario.module.scss';
+import { ITarefa } from '../../types/tarefa';
+import { v4 as uuidv4 } from 'uuid';
 
-class Formulario extends React.Component {
+class Formulario extends React.Component<{
+    setTarefas: React.Dispatch<React.SetStateAction<ITarefa[]>>
+    }> {
     state = {
         tarefa:'',
         tempo:"00:00"
@@ -10,7 +14,17 @@ class Formulario extends React.Component {
 
     adicionarTarefa(evento: React.FormEvent<HTMLFormElement>) {
         evento.preventDefault();
-        console.log('state: ', this.state);
+        this.props.setTarefas(tarefasAntigas => [
+            ...tarefasAntigas, { 
+                ...this.state,
+                selecionado: false,
+                completado: false,
+                id: uuidv4()
+            }])
+        this.setState({
+            tarefa: "",
+            tempo: "00:00"
+        })
     }
 
     render () {
